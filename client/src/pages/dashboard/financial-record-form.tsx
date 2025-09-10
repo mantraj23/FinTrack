b18687fa42448bd9b843257c1966683b2682494b
mantraj23 <mantrajain87@gmail.com>
2025-09-11 03:58:26 +0530

    modified:   client/src/pages/dashboard/financial-record-form.tsx

--- client/src/pages/dashboard/financial-record-form.tsx
+++ client/src/pages/dashboard/financial-record-form.tsx
@@ -44,7 +44,7 @@ export const FinancialRecordForm = () => {
           />
         </div>
         <div className="form-field">
-          <label>Amount:</label>
+          <label>Amount in $:</label>
           <input
             type="number"
             required
